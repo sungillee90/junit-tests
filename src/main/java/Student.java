79169e02d087bf4f6cc9b4340b37deb4e0cbf327
SUNG IL LEE <sungil.lee90@gmail.com>
2020-05-20 19:16:31 -0500

    Complete first part of the TDD exercise.

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,37 +1,52 @@
 import java.util.ArrayList;
+import java.util.Collection;
 
 public class Student {
     //The Student class should have a constructor that sets both the name and id property, it initializes the grades property as an empty ArrayList.
 
     // Created VARIABLES(properties/fields) to make the constructor.
-//    private String studentName;
     private long studentId;
-//    private ArrayList<Long> grades;
+    private String studentName;
+    private ArrayList<Integer> grades;
 
     // Constructor
-    public Student(long anId) {
-        this.studentId = anId;
-//        this.studentName = aName;
+    public Student(long id, String name) {
+        this.studentId = id;
+        this.studentName = name;
 
         // initializes the grades property as an empty ArrayList. so we don't waste memory initializing the array VERY IMPORTANT
-//        this.grades = new ArrayList<>();
+        this.grades = new ArrayList<>();
         // init!
     }
+
 
     // GETTER
     public  long getId() {
         return this.studentId;
     }
 
-//    public static String getName() {
-//        return "sung";
-//    }
-//
-//    public static int addGrade(int i) {
-//        return i;
-//    }
-//
-//    public static double getGradeAverage(double v) {
-//        return v;
-//    }
+    // GETTER
+    public String getName() {
+        return this.studentName;
+    }
+
+    public ArrayList<Integer> getGrades() {
+        return this.grades;
+    }
+
+    public void addGrade(int grade) {
+       this.grades.add(grade);
+    }
+
+    public double getAverage() {
+        if(this.grades.size() == 0) {
+            return 0;
+        }
+        double sum = 0;
+
+        for (int grade: grades) {
+            sum += grade;
+        }
+        return sum / this.grades.size();
+    }
 }
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -4,30 +4,67 @@ import static org.junit.Assert.*;
 import static org.junit.Assert.assertNotEquals;
 
 public class StudentTest {
+    // todo DRY!
+    //  Since there are Student sung = new Student again, again and again;
+    Student sung;
+
+    @Before
+    public void initializeSetting() {
+        sung = new Student (1L, "Sung");
+    }
 
     @Test
     public void testIfIdMatches() {
-        Student sung = new Student (1L);
-        // static because you want to access the CLASS
-        // expected:1000734727L   method called getID()
+        // id should be a long number used to represent a
+        // "unique user" in our application.
+//        Student sung = new Student(1L,"Sung");
+        // sung.getId() -> GETTER
         assertEquals(1L, sung.getId());
+    }
+
+    @Test
+    public void testIfNameWorks() {
+        //name is a String that holds the name of the student.
+//        Student sung = new Student(1L,"Sung");
+        assertEquals("Sung",sung.getName());
+    }
+
+    // todo ADDITIONAL TEST WAS PERFORM TO MAKE SURE
+    //  another thought process!
+    @Test
+    public void testIfGradeWorks() {
+//        sung.getGrades().size(); // should be 0 bc I did not make any grade.
+        assertEquals(0,sung.getGrades().size());
+    }
+
+    @Test
+    public void testIfAddGradeWorks() {
+        sung.addGrade(80);
+//        sung.getGrades().get(0); // 80
+        assertEquals(80.0, sung.getGrades().get(0),0);
+        assertTrue(80 ==  sung.getGrades().get(0));
+        assertFalse(90 ==  sung.getGrades().get(0));
+        // add another grade
+        sung.addGrade(100);
+        assertEquals(100, sung.getGrades().get(1),0);
+    }
+
+    @Test
+    public void testIfAvgWorks() {
+        assertEquals(0,sung.getAverage(),0);
+        sung.addGrade(100);
+        sung.addGrade(70);
+        sung.addGrade(80);
+        // 250 / 3 = 83.333
+
+        sung.getAverage();
+
+        assertEquals(83.33, sung.getAverage(), 0.1);
+
+        sung.addGrade(60);
+        assertEquals(77.5,sung.getAverage(),0.1);
 
     }
 
-//    @Test
-//    public void testIfNameMatches() {
-//        assertEquals("sung", Student.getName());
-//        assertNotEquals("Sung", Student.getName());
-//        assertSame("sung", Student.getName());
-//    }
-//
-//    @Test
-//    public void testIfGradeAdded() {
-//        assertEquals(85, Student.addGrade(85));
-//    }
-//
-//    @Test
-//    public void testIfReturnsAvg() {
-//        assertEquals(80.0, Student.getGradeAverage(80d),0);
-//    }
+
 }
